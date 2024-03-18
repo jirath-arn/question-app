d9a8e8c812ebbab07c619659ed105e45a023f3db
Jirath Arnamnath <ice191913@gmail.com>
2024-03-18 15:47:52 +0700

    Mock data

---
+++ src/data/questions.ts
@@ -0,0 +1,108 @@
+export interface Question {
+    question: string;
+    answers: string[];
+    correctAnswerIndex: number;
+}
+
+export const questions: Question[] = [
+    {
+        question: 'Choose a synonym of "Rectify"',
+        answers: ['To build', 'Correct', 'Destroy', 'To command'],
+        correctAnswerIndex: 1
+    },
+    {
+        question: 'Choose a synonym of "Parasite"',
+        answers: ['A loss of motion', 'Exterminator', 'Disease', 'One that clings'],
+        correctAnswerIndex: 3
+    },
+    {
+        question: 'Choose a synonym of "Placate"',
+        answers: ['To make sure', 'To annoy', 'To pacify', 'To flatten out'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Paramount"',
+        answers: ['Very important', 'Wide and extensive', 'Above others in rank of authority', 'Famous'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Forestall"',
+        answers: ['Avoid', 'Frighten', 'Prevent', 'Disappoint'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Acumen"',
+        answers: ['Abundance', 'Bitterness', 'Deficit', 'Quickness of insight'],
+        correctAnswerIndex: 3
+    },
+    {
+        question: 'Choose a synonym of "Nascent"',
+        answers: ['Initial', 'Unpleasant', 'Latest', 'Crude'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'Choose a synonym of "Interim"',
+        answers: ['Temporary', 'Internal', 'Timely', 'Interval'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'Choose a synonym of "Perspicacious"',
+        answers: ['Bad', 'Clear', 'Hazy', 'Shrewd'],
+        correctAnswerIndex: 3
+    },
+    {
+        question: 'Choose a synonym of "Incensed"',
+        answers: ['Affected', 'Incited', 'Encouraged', 'Inspired'],
+        correctAnswerIndex: 1
+    },
+    {
+        question: 'Choose a synonym of "Mitigate"',
+        answers: ['Send on a mission', 'Pardon', 'Soften', 'To heal'],
+        correctAnswerIndex: 3
+    },
+    {
+        question: 'Choose a synonym of "Diatribe"',
+        answers: ['Address', 'Attack', 'Discharge', 'Objection'],
+        correctAnswerIndex: 1
+    },
+    {
+        question: 'Choose a synonym of "Sensuous"',
+        answers: ['Aesthetic', 'Abundant', 'Delicious', 'Imposing'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'Choose a synonym of "Denounce"',
+        answers: ['Condemn', 'Maintain', 'Sustain', 'Support'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'Choose a synonym of "Enigmatic"',
+        answers: ['Illusive', 'Complicated', 'Puzzling', 'Pithy'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Prohibitive"',
+        answers: ['Swanky', 'Unaffordable', 'Fancy', 'Closed'],
+        correctAnswerIndex: 1
+    },
+    {
+        question: 'Choose a synonym of "Chivalrous"',
+        answers: ['Improverished', 'Impetuous', 'Gallant', 'Brave'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Venerate"',
+        answers: ['Accuse', 'Abuse', 'Respect', 'Defame'],
+        correctAnswerIndex: 2
+    },
+    {
+        question: 'Choose a synonym of "Fulsome"',
+        answers: ['Excessive', 'Liberal', 'Generous', 'Smooth'],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'Choose a synonym of "Reward"',
+        answers: ['Forfeiture', 'Demotion', 'Penalty', 'Retribution'],
+        correctAnswerIndex: 3
+    },
+];
